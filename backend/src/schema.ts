96e14a7027a06819df4ed75ff826c52268f8f873
niehusst <liamniehusstaab@gmail.com>
2025-09-30 12:54:36 -0700

    dont read schema by full path since __dirname isnt available in esm when app runs and other methods arent available in testing

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -2,10 +2,7 @@ import { readFileSync } from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-// Get the directory name in ES modules (bcus __dirname doesnt exist for some reason)
-const dirPath = __dirname; // dirname(fileURLToPath(import.meta.url));
-
 // Read the schema file
-const schema = readFileSync(join(dirPath, '../schema.graphql'), 'utf8');
+const schema = readFileSync('./schema.graphql', 'utf8');
 
 export const typeDefs = schema;
